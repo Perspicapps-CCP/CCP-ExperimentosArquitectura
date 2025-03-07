f595a754eaabd483899dfea94c63b672adbb3bcb
fclopez4 <fc.lopez@uniandes.edu.co>
2025-03-06 20:27:27 -0500

    se ajustan los deploys

--- fabricantes/config.py
+++ fabricantes/config.py
@@ -7,5 +7,5 @@ DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME","postgres_ccp_db")
+DB_NAME = os.getenv("DB_NAME","fabricantes_db")
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
